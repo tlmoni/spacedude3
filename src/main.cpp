098ef253000f5017d244b180a5ffad2b79be8ee0
Toni Ojala <toni.ojala@aalto.fi>
2019-11-01 20:20:14 +0200

    Created Makefile to src, based main.cpp, updated .gitginore

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,18 @@
-#include <iostream>
+#include <SFML/Graphics.hpp>
 
 int main() {
-    std::cout << "This is BrawlStars!" << std::endl;
+    sf::RenderWindow window(sf::VideoMode(1600, 900), "<insert game name here>");
+
+    while (window.isOpen()) {
+        sf::Event event;
+        while (window.pollEvent(event)) {
+            if (event.type == sf::Event::Closed)
+                window.close();
+        }
+
+        window.clear();
+        window.display();
+    }
+
     return 0;
 }
