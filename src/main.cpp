44942aeb009dcd88bdb53a8ed79726ab85b9740d
Simo Hakanummi <simo.hakanummi@aalto.fi>
2019-11-05 18:27:18 +0200

    Added and improved comments, small style tweaks

--- src/main.cpp
+++ src/main.cpp
@@ -2,11 +2,11 @@
 #include <SFML/Graphics.hpp>
 #include "scene.hpp"
 
-sf::RenderWindow* main_window = new sf::RenderWindow(sf::VideoMode(1600, 900), "<insert game name here>");
-Scene* scene = new Scene();
-
+/* Define main_window and scene as global variables */
+sf::RenderWindow* main_window = new sf::RenderWindow(sf::VideoMode(1600, 900), "<insert game name here>"); /* Main window, contains everything within the game*/
+Scene* scene = new Scene(); /* Contains & handles everything rendered to the main window */
+ 
 int main() {
-
     scene->Init();
 
     delete scene;
--- src/scene.cpp
+++ src/scene.cpp
@@ -3,11 +3,9 @@
 #include "scene.hpp"
 #include "player.hpp"
 
-
 /* Contructor. */
 Scene::Scene() {
     
-
 }
  /* Destructor */
 Scene::~Scene() {
@@ -19,7 +17,6 @@ void Scene::Init() {
     //Load map from file
     //Place players on map
 
-
     Player* player = new Player(sf::Vector2f(0.0f, 0.0f));
     // players_.push_back(player);
     player->Loop();
@@ -27,10 +24,9 @@ void Scene::Init() {
     delete player;
 
     End();
-
 }
 
-/* Delete objects from scene and close */
+/* Deletes objects on the scene and closes it */
 void Scene::End() {
     /*
     for (auto p : players_) {
@@ -45,7 +41,6 @@ void Scene::End() {
 /* Update game logic. Bullets etc. */
 void Scene::Update() {
 
-    
 }
 
 /* Render the game and update graphics */
