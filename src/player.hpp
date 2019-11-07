15b1232accc925d9402e036ddb17d6ed6ded872a
Simo Hakanummi <simo.hakanummi@aalto.fi>
2019-11-07 11:31:03 +0200

    Cleaned up player.hpp

--- src/player.hpp
+++ src/player.hpp
@@ -24,18 +24,16 @@ public:
     /* Function resposible for updating player character position */
     void Move(sf::Vector2f pos_dif);
 
-    /* Function returns player sprite */
-    //sf::RectangleShape GetSprite() { return sprite_; } // Disabled for testing actual sprite with textures
-    sf::Sprite GetSprite() { return sprite_; }
+    sf::Sprite GetSprite() { return sprite_; } // Returns player sprite
 
-    sf::Vector2f GetPosition() {return pos_; }
+    sf::Vector2f GetPosition() {return pos_; } // Return player position
 
 
     /* UNDER CONSTRUCTION: Player sprite turning with mouse */
 
-    sf::Vector2f GetDirection() {return direction_cursor_; }
+    sf::Vector2f GetDirection() {return direction_cursor_; } // Return mousewise direction, relative to player sprite.
 
-    /* Function that calculates current mousewise direction of the player sprite */
+    /* Function that calculates current mousewise direction, relative to player sprite */
     sf::Vector2f GetCurrentCursorDirection();
 
     /* UNDER CONSTRUCTION END */
@@ -43,15 +41,14 @@ public:
 
 private:
     sf::Vector2f pos_; // Variable tracking player position the scene
-    //sf::RectangleShape sprite_; // Variable to hold player sprite, disabled for testing actual sprite with textures
     sf::Sprite sprite_; // Variable to hold player sprite
-
-    /* UNDER CONSTRUCTION: Add variables to hold potential sprites loaded from files here */
     sf::Texture texture_basic_; // Holds player texture
     sf::Vector2u texture_size_; // Holds player sprite size
-    enum Direction {LEFT, RIGHT, UP, DOWN, DEFAULT}; // Enumerator for directions
-    int direction_; // Holds current direction the player sprite is facing towards
-    sf::Vector2f direction_cursor_; // Holds current direction the mouse is point to, relative to player sprite
+
+    /* UNDER CONSTRUCTION: Add variables to hold potential sprites loaded from files here */
+    enum Direction {LEFT, RIGHT, UP, DOWN, DEFAULT}; // KEYBAORD: Enumerator for directions
+    int direction_; // KEYBOARD: Holds current direction the player sprite is facing towards
+    sf::Vector2f direction_cursor_; // MOUSE: Holds mousewise direction, relative to player sprite
 
     /* UNDER CONSTRUCTION END */
 
