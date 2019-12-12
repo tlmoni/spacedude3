c38569b22095a93c2062612cdc9feb7144f49f0a
Toni Ojala <toni.ojala@aalto.fi>
2019-12-12 21:15:54 +0200

    Made goal to be necessary in campaign maps

--- src/character_spacedude.hpp
+++ src/character_spacedude.hpp
@@ -8,5 +8,6 @@
 class SpaceDude : public Character {
 public:
     SpaceDude() : Character("src/Textures/spacedudebody.png", RectHitbox(50.f, 50.f), "Spacedude", 5.0f, 1.0f, 2.f, 200, 100, PhysicsVector(26,32)) { }
+
 private:
 };
