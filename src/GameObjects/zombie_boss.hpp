79d5055cd339237b43815703f3375600842ae961
Mikko <mikko.suhonen1@hotmail.com>
2019-12-13 18:00:50 +0200

    fixed master

--- src/GameObjects/zombie_boss.hpp
+++ src/GameObjects/zombie_boss.hpp
@@ -8,11 +8,7 @@ public:
     ZombieBoss(PhysicsVector pos) : Zombie(pos) {
         SetMaxHitPoints(5000.f);
         SetAttackDelay(3000);
-<<<<<<< HEAD
         SetMaxSpeed(2.7f);
-        SetSprite("src/Textures/zombieboss.png");
-        GetSprite().setScale(1.5f, 1.5f);
-=======
         SetMaxSpeed(2);
         int random_integer = rand() % 3;
         if (random_integer == 0) {
@@ -25,7 +21,6 @@ public:
             SetSprite("src/Textures/zombieboss3.png");
         }
         GetSprite().setScale(1.5f,1.5f);
->>>>>>> fd7c7ffa49b058a074b44291f596ab19bba6d778
         SetWeapon(skull);
         SetAttackRange(300);
 
