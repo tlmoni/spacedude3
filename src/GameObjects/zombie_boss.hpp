06c2f56a2dc24aaf5dc21d2fbe9a9c273af2d903
Simo Hakanummi <simo.hakanummi@aalto.fi>
2019-12-13 17:30:28 +0200

    Added MORSSARI into the game, you'll know what I mean

--- src/GameObjects/zombie_boss.hpp
+++ src/GameObjects/zombie_boss.hpp
@@ -6,9 +6,12 @@ class ZombieBoss : public Zombie {
 public:
     /* Constructor */
     ZombieBoss(PhysicsVector pos) : Zombie(pos) {
-        SetMaxHitPoints(1000.f);
+        SetMaxHitPoints(3000.f);
         SetAttackDelay(3000);
         SetMaxSpeed(2);
+        SetSprite("src/Textures/zombieboss.png");
+        GetSprite().setScale(1.5f, 1.5f);
+        SetWeapon(skull);
 
     }
 
